74480fe4df594b2297900eadcd6fd63426019d56
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 14:23:40 -0400

    Fixed the testTwo() test.

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class PrimeFactors {
     public static List<Integer> generate(int i) {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        if (i > 1) {
+            primes.add(2);
+        }
+        return primes;
     }
 }
