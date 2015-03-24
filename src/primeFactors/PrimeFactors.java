20a09c2f7b3c91521faaa12c70cd335128ec0d7d
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 14:20:00 -0400

    Fixed the failing test testOne() in the previous commit.

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -1,9 +1,10 @@
 package primeFactors;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static List<Integer> generate(int i) {
-        return null;
+        return new ArrayList<Integer>();
     }
 }
