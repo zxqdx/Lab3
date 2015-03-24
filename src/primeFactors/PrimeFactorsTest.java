129789abda6e4b6f7538897ff3444b720c822ef2
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 14:22:11 -0400

    Added a failing test testTwo().

--- src/primeFactors/PrimeFactorsTest.java
+++ src/primeFactors/PrimeFactorsTest.java
@@ -9,13 +9,18 @@ import static org.junit.Assert.*;
 
 public class PrimeFactorsTest {
 
-    private List<Integer> list() {
-        return Arrays.asList();
+    private List<Integer> list(Integer...integers) {
+        return Arrays.asList(integers);
     }
 
     @Test
     public void testOne() {
         assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(list(2), PrimeFactors.generate(2));
     }
 
 }
