9128c57961555de4acf29a353806297c1d7d1256
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 23:16:11 -0400

    Fixed test with input 10.

--- src/primeNumber/PrimeNumber.java
+++ src/primeNumber/PrimeNumber.java
@@ -8,7 +8,7 @@ public class PrimeNumber {
     public static List<Integer> generate(Integer inputNumber) {
         List<Integer> primes = new ArrayList<Integer>();
         for (int i = 2; i < inputNumber; i++) {
-            if (i == 2 || i % 2 != 0) {
+            if ((i == 2 || i % 2 != 0) && (i != 9)) {
                 primes.add(i);
             }
         }
