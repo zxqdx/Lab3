6bc4d741edcd539a1f2e3bcd7adb516fa15319c1
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 23:16:11 -0400

    Refactor code in PrimeNumber.generate().

--- src/primeNumber/PrimeNumber.java
+++ src/primeNumber/PrimeNumber.java
@@ -1,14 +1,16 @@
 package primeNumber;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PrimeNumber {
 
     public static List<Integer> generate(Integer inputNumber) {
-        if (inputNumber == 3) {
-            return Arrays.asList(2);
+        List<Integer> primes = new ArrayList<Integer>();
+        if (inputNumber > 2) {
+            primes.add(2);
         }
-        return Arrays.asList();
+        return primes;
     }
 }
