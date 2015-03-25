fbc8716349d1f99af1021d9cf2e5d3f195642efc
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 22:38:22 -0400

    Fixed code for test testThree().

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -7,7 +7,7 @@ public class PrimeFactors {
     public static List<Integer> generate(int i) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
         if (i > 1) {
-            primes.add(2);
+            primes.add(i);
         }
         return primes;
     }
