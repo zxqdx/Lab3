3ee0ca6f55af4bf4ce4f6abfc1fd7aa85286af13
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 22:41:55 -0400

    Added test testSix() which happens to pass.

--- src/primeFactors/PrimeFactorsTest.java
+++ src/primeFactors/PrimeFactorsTest.java
@@ -34,4 +34,9 @@ public class PrimeFactorsTest {
         assertEquals(list(2, 2), generate(4));
     }
 
+    @Test
+    public void testSix() {
+        assertEquals(list(2, 3), generate(6));
+    }
+
 }
