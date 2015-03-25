9dd9fbb551ed8122aea1de371059d771943584f3
zxqdx <acfun.zxqdx@gmail.com>
2015-03-24 22:40:25 -0400

    Fixed test testFour().

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> generate(int i) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
+        if (i % 2 == 0) {
+            primes.add(2);
+            i /= 2;
+        }
         if (i > 1) {
             primes.add(i);
         }
